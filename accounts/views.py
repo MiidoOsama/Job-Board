3c71209722d76f4f3faf248a50997925052e4507
MiidoOsama <xmiido@yandex.com>
2024-02-01 22:56:46 +0200

    added all registeration and profile processes

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import SignupForm
+from django.contrib.auth import authenticate , login
+from .models import Profile
 
 def signup(request):
-    
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password =  form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('/accounts/profile')
 
-    return render(request, 'registration/signup.html')
+    else:
+        form = SignupForm()
+    
+    return render(request, 'registration/signup.html' , {'form':form})
+
+
+
+def profile(request):
+    profile = Profile.objects.get(user = request.user)
+    return render(request , 'accounts/profile.html' , {'profile' : profile})
+
+
+def profile_edit(request):
+    
+    
+    return render(request , 'accounts/profile_edit.html', {})
